0cee36047f4d1758f62f0e40884729bc8f9c4825
mohamedHamdy011574 <mohamedhamdydeveloper55@gmail.com>
2025-07-05 03:42:17 +0300

    test storing products process

--- App.java
+++ App.java
@@ -9,7 +9,7 @@ public class App {
           /*    test expire date error by edit the expire date to be in past, 
                 test not enough quantity error by edit the quantity 
           */
-          Product cheese = new ExpirableShippableProduct("Cheese", 100, 10, 0.4, "2025-07-05");
+          Product cheese = new ExpirableShippableProduct("Cheese", 100, 10, 0.4, "2025-07-04");
           Product biscuits = new ExpirableShippableProduct("Biscuits", 150, 5, 0.7, "2025-07-07");
           Product tv = new ShippableProduct("TV", 5000, 3, 10);
           Product scratchCard = new Product("Scratch Card", 50, 20);
@@ -17,6 +17,10 @@ public class App {
           Customer customer = new Customer("Ali", 10000);
 
           System.out.println(customer.getName());
+          System.out.println(cheese.getName());
+          System.out.println(biscuits.getName());
+          System.out.println(tv.getName());
+          System.out.println(scratchCard.getName());
 
         }catch(Exception e){
             System.err.println(e.getMessage());

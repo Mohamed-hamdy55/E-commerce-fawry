fef432ccc7a0d7544c43be10bd3890faae8c736a
mohamedHamdy011574 <mohamedhamdydeveloper55@gmail.com>
2025-07-05 03:33:20 +0300

    init commit

---
+++ customer/Customer.java
@@ -0,0 +1,26 @@
+package customer;
+
+public class Customer {
+    private String name;
+    private double balance;
+
+    public Customer(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deduct(double amount) {
+        if (balance < amount) {
+            throw new IllegalArgumentException("Insufficient balance");
+        }
+        balance -= amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
